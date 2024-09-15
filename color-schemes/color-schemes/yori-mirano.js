f47b5b6e31dc5caa272867dfc91130c72dfd83be
Yori Mirano <1879222+Yori-Mirano@users.noreply.github.com>
2024-09-15 16:14:09 +0200

    Create yori-mirano.js theme

---
+++ color-schemes/color-schemes/yori-mirano.js
@@ -0,0 +1,38 @@
+colorscheme.addScheme("yori-mirano", {
+  "name": "Yori Mirano",
+  "background": "#000000",
+  "nodes": {
+    "OFF": {
+      "label": "#969696",
+      "fill": "#3c3c3c",
+      "stroke": "#3c3c3c"
+    },
+    "GHOST": {
+      "label": "#a3a3a3",
+      "fill": "#595959",
+      "stroke": "#636363"
+    },
+    "SUSTAIN": {
+      "label": "#001624",
+      "fill": "#7ab5d6",
+      "stroke": "#7ab5d6"
+    },
+    "ON": {
+      "label": "#3b2802",
+      "fill": "#ffc966",
+      "stroke": "#ffc966"
+    }
+  },
+  "faces": {
+    "major": {
+      "label-off": "#000000",
+      "label-on": "#e3e3e3",
+      "fill": "#666666"
+    },
+    "minor": {
+      "label-off": "#000000",
+      "label-on": "#b5b5b5",
+      "fill": "#454545"
+    }
+  }
+});
